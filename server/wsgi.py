cf3f0fbb28b265eb31856ba7b2ba7853627f1932
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2019-12-02 21:33:10 +0000

    yn rhoi trawsgrifiad i ffeil wav yn lwyddianus

--- server/wsgi.py
+++ server/wsgi.py
@@ -2,8 +2,9 @@
 import os
 import sys
 import glob
-
+import wave
 import cherrypy
+import numpy as np
 
 from datetime import datetime
 from deepspeech import Model
@@ -29,9 +30,10 @@ class DeepSpeechAPI(object):
         self.tmp_dir = '/tmp'
 
         output_graph, alphabet, lm, trie = resolve_models(DEEPSPEECH_MODEL_DIR)
-
+        cherrypy.log("Loading DeepSpeech model....")
         self.ds = Model(output_graph, N_FEATURES, N_CONTEXT, alphabet, BEAM_WIDTH)
         self.ds.enableDecoderWithLM(alphabet, lm, trie, LM_ALPHA, LM_BETA)
+        cherrypy.log("Loading DeepSpeech model completed")
 
 
     @cherrypy.expose
@@ -43,7 +45,7 @@ class DeepSpeechAPI(object):
     @cherrypy.expose
     @cherrypy.tools.json_out(handler=callback_handler)
     def speech_to_text(self, soundfile, **kwargs):
-        upload_tmp_filepath = os.path.join(self.tmp_dir, 'ds_request_' + datetime.now().strftime('%y-%m-%d_%H%M%S'))
+        upload_tmp_filepath = os.path.join(self.tmp_dir, 'ds_request_' + datetime.now().strftime('%y-%m-%d_%H%M%S') + '.wav')
         with open(upload_tmp_filepath, 'wb') as wavfile:
             while True:
                 data = soundfile.file.read(8192)
@@ -54,25 +56,53 @@ class DeepSpeechAPI(object):
         result = {
                 'version':1
         }
-        result.update({'text': self.ds.stt(upload_tmp_filepath, 16000)})
+
+        #
+        success = True
+        fin = wave.open(upload_tmp_filepath, 'rb')
+        fs = fin.getframerate()
+        if fs != 16000:
+            success = False
+
+        audio = np.frombuffer(fin.readframes(fin.getnframes()), np.int16)
+        fin.close()
+
+        text = self.ds.stt(audio, fs)
+        if len(text)==0:
+            success=False
+
+        result.update({
+            'text': text,
+            'success': success
+        })
+
         return result
 
 
 
 def resolve_models(dirName):
     pb = glob.glob(dirName + "/*.pb")[0]
+    cherrypy.log("model file found: %s" % pb)
+
     alphabet = glob.glob(dirName + "/alphabet.txt")[0]
+    cherrypy.log("model file found: %s" % alphabet)
+    
     lm = glob.glob(dirName + "/lm.binary")[0]
+    cherrypy.log("model file found: %s" % lm)
+    
     trie = glob.glob(dirName + "/trie")[0]
+    cherrypy.log("model file found: %s" % trie)
 
     return pb, alphabet, lm, trie
+
 
 
 cherrypy.config.update({
     'environment': 'production',
     'log.screen': False,
     'response.stream': True,
-    'log.error_file': 'deepspeech-server.log',
+    'log.access_file': 'deepspeech-access.log',
+    'log.error_file': 'deepspeech-error.log',
 })
 
 cherrypy.tree.mount(DeepSpeechAPI(), '/')
